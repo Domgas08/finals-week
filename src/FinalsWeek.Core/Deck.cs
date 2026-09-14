febb7a27845a092f4951af6dc0d96800ffba4234
Domgas08 <domas.grimalauskas@gmail.com>
2026-09-14 16:37:39 +0300

    Added proper layout of the projects, missing project files / folders and stylecop with proper configurations for uniform codestyle

--- src/FinalsWeek.Core/Deck.cs
+++ src/FinalsWeek.Core/Deck.cs
@@ -3,31 +3,31 @@
 public enum Visibility
 {
     Public,
-    Private
+    Private,
 }
+
 public class Deck
 {
-    public string Title {get; private set;}
-    public string? CourseCode {get; set;}
-    public string? Description {get; set;}
-    public Visibility Visibility {get; set;}
+    public string Title { get; private set; }
+
+    public string? CourseCode { get; set; }
+
+    public string? Description { get; set; }
+
+    public Visibility Visibility { get; set; }
 
     public Deck(string title, string? courseCode = null, string? description = null, Visibility visibility = Visibility.Private)
     {
-        
-
         this.Title = ValidateTitle(title);
         this.CourseCode = courseCode;
         this.Description = description;
         this.Visibility = visibility;
     }
 
-        public void Rename(string newTitle)
+    public void Rename(string newTitle)
     {
-        
         this.Title = ValidateTitle(newTitle);
     }
-
 
     private static string ValidateTitle(string title)
     {
@@ -36,12 +36,12 @@ public class Deck
         {
             throw new ArgumentException("Title cannot be empty");
         }
-        if ((title.Length > 120))
+
+        if (title.Length > 120)
         {
             throw new ArgumentException("Title cannot be longer than 120 characters");
         }
+
         return title;
     }
-
-
 }
