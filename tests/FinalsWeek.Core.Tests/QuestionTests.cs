6ee467c75b7a49d9e49e92e7235336fb2bd0a5a2
fo-klimas <feliksas.oskaras.klimas@gmail.com>
2026-09-22 18:52:28 +0300

    Added Invalid time limit and Question immutability unit tests

--- tests/FinalsWeek.Core.Tests/QuestionTests.cs
+++ tests/FinalsWeek.Core.Tests/QuestionTests.cs
@@ -50,4 +50,28 @@ public class QuestionTests
 
         Assert.Throws<ArgumentException>(() => new Question(prompt, options, 0));
     }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    [InlineData(4)]
+    public void Constructor_WithInvalidTimeLimit_ThrowsArgumentOutOfRangeException(int invalidTimeLimit)
+    {
+        string prompt = "What is 2 + 2?";
+        var options = new List<string> { "3", "4", "5" };
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Question(prompt, options, 1, timeLimitSeconds: invalidTimeLimit));
+    }
+
+    [Fact]
+    public void Question_IsImmutable_ModificationCreatesNewInstanceAndOriginalOptionsCannotBeMutated()
+    {
+        var options = new List<string> { "Vilnius", "Kaunas", "Klaipeda" };
+        var question = new Question("Capital of Lithuania?", options, 0, "Geography", 10);
+
+        options.Add("Siauliai");
+
+        Assert.Equal(3, question.Options.Count);
+        Assert.DoesNotContain("Siauliai", question.Options);
+    }
 }
